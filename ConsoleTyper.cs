a4567c9e73e006ca26e4206a0dcc16c89c53fe7d
BAC <41997195+bac0id@users.noreply.github.com>
2020-09-09 16:59:02 +0800

    Add files via upload

---
+++ ConsoleTyper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TextTyper
+{
+	public class ConsoleTyper : Typer
+	{
+		public ConsoleTyper(float charPerSecond = DefaultTypingSpeed, float mistakeRate = DefaultMistakeRate)
+			: base(charPerSecond, mistakeRate) {
+		}
+
+		public override void WriteChar(char c) {
+			Console.Write(c);
+		}
+
+		public override void EraseLastWord(string lastTypedWord) {
+			Console.CursorLeft++;
+			for (int i = 0; i < lastTypedWord.Length; ++i) {
+				Console.CursorLeft -= 2;
+				// these 2 sentenses can be replaced by PressKeyboard(' ');
+				// but the typer doesn't really press the SPACE key.
+				Console.Write(' ');
+				base.Sleep();
+			}
+			Console.CursorLeft--;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TextTyper
+{
+	public class Program
+	{
+		static void Main(string[] args) {
+			TyperTest test = new TyperTest();
+			test.TestTyper();
+			//Console.WriteLine();
+			//test.TestChangeTypingSpeed();
+			//Console.WriteLine();
+			//test.TestChangeMistakeRate();
+
+			Console.ReadKey();
+		}
+	}
+
+	public class TyperTest
+	{
+		public Typer typer = new ConsoleTyper(20f,0.3f);
+		public void TestTyper() {
+			typer.TypeIn("Hi, I'm a typist.\n");
+			typer.TypeIn("I can use keyboard well but sometimes I mistake the word.\n");
+		}
+		public void TestChangeTypingSpeed() {
+			typer.CharPerSecond = 40f;
+			typer.TypeIn("Licensing a repository\n");
+			typer.TypeIn("Public repositories on GitHub are often used to share open source software. ");
+			typer.TypeIn("For your repository to truly be open source, you'll need to license it so that others are free to use, change, and distribute the software.\n");
+		}
+		public void TestChangeMistakeRate() {
+			typer.MistakeRate = 0.5f;
+			typer.TypeIn("C# is an object-oriented, component-oriented programming language.\n");
+			typer.TypeIn("C# provides language constructs to directly support these concepts, making C# a natural language in which to create and use software components.");
+		}
+	}
+}
